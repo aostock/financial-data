a23faa88218226c720d56b545c5ac36e5ec8f4d7
lz <ohyear09@gmail.com>
2025-08-14 15:43:16 +0800

    change port

--- main.py
+++ main.py
@@ -314,6 +314,6 @@ mcp.mount_sse()
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("--port", 8000))
+    port = int( os.environ.get("PM2_SERVE_PORT", 8000))
     print(f"Starting FastAPI app with MCP on port {port}")
     uvicorn.run(app, host="0.0.0.0", port=port)
